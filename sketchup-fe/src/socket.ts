693d3c61fa1e7cbd3c2a1893b1ba2a95034c3e60
AbhinavKr0110 <abhinavkr2345@gmail.com>
2026-07-15 15:33:55 +0530

    Configure production websocket endpoints

--- sketchup-fe/src/socket.ts
+++ sketchup-fe/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_SERVER_URL = 'http://localhost:4000';
+const SOCKET_SERVER_URL = 'https://sketchup-backend-ukat.onrender.com';
 
 export const socket: Socket = io(SOCKET_SERVER_URL, {
     autoConnect: false,
